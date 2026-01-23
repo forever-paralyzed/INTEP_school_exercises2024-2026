d3eab0f0ba43a1046979ecc75cefb31a5f779184
WasntAwesome <67332887+svyat777@users.noreply.github.com>
2026-01-23 15:31:39 +0000

    Add files via upload

---
+++ exercise_27.java
@@ -0,0 +1,19 @@
+package sass;
+public class exercise_27 {
+
+    public static void main(String[] args) {
+        
+        String value_to_convert = "10";
+        System.out.println("value in string: " + value_to_convert);
+
+        int converted_to_int = Integer.parseInt(value_to_convert);
+        System.out.println("value in int: " + converted_to_int);
+
+        float converted_to_float = Float.parseFloat(value_to_convert);
+        System.out.println("value in float: " + converted_to_float);
+
+
+    }
+}
+
+
